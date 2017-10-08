b478dd2d002959d4f3a3b608720a29c2a13bf715
Chris Want <cjwant@gmail.com>
2017-10-07 21:22:44 -0600

    SquareTessagon (good for testing stuff).

---
+++ square_tessagon.py
@@ -0,0 +1,45 @@
+from tessagon import Tessagon
+from tile import Tile
+
+class SquareTile(Tile):
+  def init_verts(self):
+    return { 'lefttop': None,
+             'leftbottom': None,
+             'rightbottom': None,
+             'righttop': None
+    }
+
+  def calculate_verts(self):
+    self.lefttop_vert()
+    self.leftbottom_vert()
+    self.rightbottom_vert()
+    self.righttop_vert()
+
+  def lefttop_vert(self):
+    vert = self.add_vert('lefttop', *self.blend(0, 0))
+    self.set_equivalent_vert(['left'], 'righttop', vert)
+
+  def leftbottom_vert(self):
+    vert = self.add_vert('leftbottom', *self.blend(0, 1))
+    self.set_equivalent_vert(['left'], 'rightbottom', vert)
+
+  def rightbottom_vert(self):
+    vert = self.add_vert('rightbottom', *self.blend(1, 1))
+    self.set_equivalent_vert(['right'], 'leftbottom', vert)
+
+  def righttop_vert(self):
+    vert = self.add_vert('righttop', *self.blend(1, 0))
+    self.set_equivalent_vert(['right'], 'lefttop', vert)
+
+  def init_faces(self):
+    return { 'middle': None }
+
+  def calculate_faces(self):
+    self.add_face('middle', [self.get_vert('lefttop'),
+                             self.get_vert('leftbottom'),
+                             self.get_vert('rightbottom'),
+                             self.get_vert('righttop')])
+
+class SquareTessagon(Tessagon):
+  def init_tile_class(self):
+    return SquareTile
