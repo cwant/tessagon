2b2b140a96b504976387c37ca676821c7da40702
Chris Want <cjwant@gmail.com>
2017-10-14 16:12:02 -0600

    Convert HexSquareTriTessagon

--- types/hex_square_tri_tessagon.py
+++ types/hex_square_tri_tessagon.py
@@ -20,43 +20,50 @@ class HexSquareTriTile(Tile):
   #     ...|...|...  6..|.4.|..6
   #
   #       U ---->
+
+  def __init__(self, tessagon, **kwargs):
+    super().__init__(tessagon, **kwargs)
+    self.u_symmetric = True
+    self.v_symmetric = True
+
   def init_verts(self):
-    return { 'left': { 'top': None,
-                       'uppermiddle': None,
-                       'lowermiddle': None,
-                       'bottom': None },
-             'right': { 'top': None,
-                        'uppermiddle': None,
-                        'lowermiddle': None,
-                        'bottom': None },
-             'top': { 'left': None,
-                      'middle': None,
-                      'right': None },
-             'bottom': { 'left': None,
-                         'middle': None,
-                         'right': None }
-    }
+    # u_square means on the square that is on the U-boundary
+    return { 'top': {'left': {'u_boundary': None,
+                              'u_square': None,
+                              'v_square': None},
+                     'right': {'u_boundary': None,
+                               'u_square': None,
+                               'v_square': None},
+                     'center': None },
+             'bottom': {'left': {'u_boundary': None,
+                                 'u_square': None,
+                                 'v_square': None},
+                        'right': {'u_boundary': None,
+                                  'u_square': None,
+                                  'v_square': None},
+                        'center': None } }
 
   def init_faces(self):
-    return { 'hex': { 'lefttop': None,
-                      'righttop': None,
-                      'middle': None,
-                      'leftbottom': None,
-                      'rightbottom': None },
-             'tri': { 'top': None,
-                      'lefttop': None,
-                      'righttop': None,
-                      'leftbottom': None,
-                      'rightbottom': None,
-                      'bottom': None },
-             'square': { 'top': None,
-                         'lefttop': None,
-                         'righttop': None,
-                         'left': None,
-                         'right': None,
-                         'leftbottom': None,
-                         'rightbottom': None,
-                         'bottom': None } }
+    # Whelp!
+    return { 'hex': { 'top': { 'left': None,
+                               'right': None },
+                      'bottom': { 'left': None,
+                                  'right': None },
+                      'middle': None },
+             'tri': { 'top': { 'left': None,
+                               'center': None,
+                               'right': None },
+                      'bottom': { 'left': None,
+                                  'center': None,
+                                  'right': None } },
+             'square': { 'top': { 'left': None,
+                                  'center': None,
+                                  'right': None },
+                         'bottom': { 'left': None,
+                                     'center': None,
+                                     'right': None },
+                         'middle': { 'left': None,
+                                     'right': None } } }
 
   def calculate_verts(self):
     # u_unit is the length of the edges expressed as a proportion of the tile
@@ -65,160 +72,73 @@ class HexSquareTriTile(Tile):
     u1 = 0.5*u_unit
     u2 = 0.5*(1.0-u_unit)
     u3 = 0.5
-    u4 = 0.5*(1.0+u_unit)
-    u5 = 1.0 - 0.5*u_unit
-    u6 = 1
 
     v_unit = 1.0 / (3.0 + sqrt(3))
     v0 = 1.0 - 0.5*v_unit
     v1 = 1.0 - v_unit
     v2 = 0.5 + v_unit
     v3 = 1.0 - 2.0*v_unit
-    v4 = 2.0*v_unit
-    v5 = 0.5 - v_unit
-    v6 = v_unit
-    v7 = 0.5*v_unit
 
-    # Top triangle
-    self.add_vert(['top', 'left'], *self.blend(u2, v0))
-    self.add_vert(['top', 'middle'], *self.blend(u3, v2))
-    self.add_vert(['top', 'right'], *self.blend(u4, v0))
-
-    # Bottom triangle
-    self.add_vert(['bottom', 'left'], *self.blend(u2, v7))
-    self.add_vert(['bottom', 'middle'], *self.blend(u3, v5))
-    self.add_vert(['bottom', 'right'], *self.blend(u4, v7))
-
-    # Left verts
-    vert = self.add_vert(['left','top'], *self.blend(u0, v1))
-    self.set_equivalent_vert(['left'], ['right', 'top'], vert)
-    self.add_vert(['left','uppermiddle'], *self.blend(u1, v3))
-    self.add_vert(['left','lowermiddle'], *self.blend(u1, v4))
-    vert = self.add_vert(['left','bottom'], *self.blend(u0, v6))
-    self.set_equivalent_vert(['left'], ['right', 'bottom'], vert)
-
-    # Right verts
-    vert = self.add_vert(['right','top'], *self.blend(u6, v1))
-    self.set_equivalent_vert(['right'], ['left', 'top'], vert)
-    self.add_vert(['right','uppermiddle'], *self.blend(u5, v3))
-    self.add_vert(['right','lowermiddle'], *self.blend(u5, v4))
-    vert = self.add_vert(['right','bottom'], *self.blend(u6, v6))
-    self.set_equivalent_vert(['right'], ['left', 'bottom'], vert)
+    # Define top left square, other verts defined through symmetry
+    self.add_vert(['top', 'left', 'v_square'], u2, v0)
+    self.add_vert(['top', 'center'], u3, v2)
+    self.add_vert(['top','left', 'u_square'], u1, v3)
+    self.add_vert(['top','left', 'u_boundary'], u0, v1, u_boundary=True)
 
   def calculate_faces(self):
-    self.outer_faces()
-    self.inner_faces()
+    # Middle hexagon
+    self.add_face(['hex', 'middle'],
+                  [['top', 'center'],
+                   ['top', 'left', 'u_square'],
+                   ['bottom', 'left', 'u_square'],
+                   ['bottom', 'center'],
+                   ['bottom', 'right', 'u_square'],
+                   ['top', 'right', 'u_square']],
+                  face_type='hexagon')
 
-  def outer_faces(self):
-    # Left outer faces
-    self.left_outer_faces()
-    # Right outer faces
-    tile = self.get_neighbor_path(['right'])
-    if tile:
-      tile.left_outer_faces()
+    ### Six top-left faces, rest defined via symmetry
     # Top square
-    self.top_square()
-    # Bottom square
-    tile = self.get_neighbor_path(['bottom'])
-    if tile:
-      tile.top_square()
-
-  def left_outer_faces(self):
-    # Two hexes
-    self.left_top_corner_hex()
-    tile = self.get_neighbor_path(['bottom'])
-    if tile:
-      tile.left_top_corner_hex()
-    # Two triangles
-    verts = [self.get_vert(['left', 'top']),
-             self.get_vert(['left', 'uppermiddle']),
-             self.get_neighbor_vert(['left'], ['right', 'uppermiddle'])]
-    face = self.add_face(['tri', 'lefttop'], verts, face_type='triangle')
-    self.set_equivalent_face(['left'], ['tri', 'righttop'], face)
-    verts = [self.get_vert(['left', 'bottom']),
-            self.get_vert(['left', 'lowermiddle']),
-             self.get_neighbor_vert(['left'], ['right', 'lowermiddle'])]
-    face = self.add_face(['tri', 'leftbottom'], verts, face_type='triangle')
-    self.set_equivalent_face(['left'], ['tri', 'rightbottom'], face)
-    # One square
-    verts = [self.get_vert(['left', 'uppermiddle']),
-             self.get_vert(['left', 'lowermiddle']),
-             self.get_neighbor_vert(['left'], ['right', 'lowermiddle']),
-             self.get_neighbor_vert(['left'], ['right', 'uppermiddle'])]
-    face = self.add_face(['square', 'left'], verts, face_type='square')
-    self.set_equivalent_face(['left'], ['square', 'right'], face)
-
-  def left_top_corner_hex(self):
-    verts = [self.get_vert(['top', 'left']),
-             self.get_vert(['left', 'top']),
-             self.get_neighbor_vert(['left'], ['top', 'right']),
-             self.get_neighbor_vert(['left', 'top'], ['bottom', 'right']),
-             self.get_neighbor_vert(['top'], ['left', 'bottom']),
-             self.get_neighbor_vert(['top'], ['bottom', 'left'])]
-
-    face = self.add_face(['hex', 'lefttop'], verts, face_type='hexagon')
-    self.set_equivalent_face(['left'], ['hex', 'righttop'], face)
-    self.set_equivalent_face(['top', 'left'], ['hex', 'rightbottom'], face)
-    self.set_equivalent_face(['top'], ['hex', 'leftbottom'], face)
-
-  def top_square(self):
-    verts = [self.get_vert(['top', 'left']),
-             self.get_vert(['top', 'right']),
-             self.get_neighbor_vert(['top'], ['bottom', 'right']),
-             self.get_neighbor_vert(['top'], ['bottom', 'left'])]
-
-    face = self.add_face(['square', 'top'], verts, face_type='square')
-    self.set_equivalent_face(['top'], ['square', 'bottom'], face)
-
-  def inner_faces(self):
-    # Hexagon
-    verts = [self.get_vert(['top', 'middle']),
-             self.get_vert(['left', 'uppermiddle']),
-             self.get_vert(['left', 'lowermiddle']),
-             self.get_vert(['bottom', 'middle']),
-             self.get_vert(['right', 'lowermiddle']),
-             self.get_vert(['right', 'uppermiddle'])]
-    self.add_face(['hex', 'middle'], verts, face_type='hexagon')
-
-    # Top triangle
-    verts = [self.get_vert(['top', 'left']),
-             self.get_vert(['top', 'right']),
-             self.get_vert(['top', 'middle'])]
-    self.add_face(['tri', 'top'], verts, face_type='triangle')
-
-    # Bottom triangle
-    verts = [self.get_vert(['bottom', 'left']),
-             self.get_vert(['bottom', 'right']),
-             self.get_vert(['bottom', 'middle'])]
-    self.add_face(['tri', 'bottom'], verts, face_type='triangle')
-
-    # Lefttop square
-    verts = [self.get_vert(['top', 'left']),
-             self.get_vert(['top', 'middle']),
-             self.get_vert(['left', 'uppermiddle']),
-             self.get_vert(['left', 'top'])]
-    self.add_face(['square', 'lefttop'], verts, face_type='square')
-
-    # Righttop square
-    verts = [self.get_vert(['top', 'right']),
-             self.get_vert(['top', 'middle']),
-             self.get_vert(['right', 'uppermiddle']),
-             self.get_vert(['right', 'top'])]
-    self.add_face(['square', 'righttop'], verts, face_type='square')
-
-    # Leftbottom square
-    verts = [self.get_vert(['bottom', 'left']),
-             self.get_vert(['bottom', 'middle']),
-             self.get_vert(['left', 'lowermiddle']),
-             self.get_vert(['left', 'bottom'])]
-    self.add_face(['square', 'leftbottom'], verts, face_type='square')
-
-    # Rightbottom square
-    verts = [self.get_vert(['bottom', 'right']),
-             self.get_vert(['bottom', 'middle']),
-             self.get_vert(['right', 'lowermiddle']),
-             self.get_vert(['right', 'bottom'])]
-    self.add_face(['square', 'rightbottom'], verts, face_type='square')
+    self.add_face(['square', 'top', 'center'],
+                  [['top', 'left', 'v_square'],
+                   ['top', 'right', 'v_square'],
+                   [['top'], ['bottom', 'right', 'v_square']],
+                   [['top'], ['bottom', 'left', 'v_square']]],
+                  face_type='square', v_boundary=True)
+    # Left square
+    self.add_face(['square', 'middle', 'left'],
+                  [['top', 'left', 'u_square'],
+                   ['bottom', 'left', 'u_square'],
+                   [['left'], ['bottom', 'right', 'u_square']],
+                   [['left'], ['top', 'right', 'u_square']]],
+                  face_type='square', u_boundary=True)
+    # Interior square
+    self.add_face(['square', 'top', 'left'],
+                  [['top', 'left', 'v_square'],
+                   ['top', 'center'],
+                   ['top', 'left', 'u_square'],
+                   ['top', 'left', 'u_boundary']],
+                  face_type='square')
+    # Upper triangle
+    self.add_face(['tri', 'top', 'center'],
+                  [['top', 'center'],
+                   ['top', 'left', 'v_square'],
+                   ['top', 'right', 'v_square']],
+                  face_type='triangle')
+    # Left triangle
+    self.add_face(['tri', 'top', 'left'],
+                  [['top', 'left', 'u_square'],
+                   ['top', 'left', 'u_boundary'],
+                   [['left'], ['top', 'right', 'u_square']]],
+                  face_type='triangle', u_boundary=True)
+    # Corner hexagon
+    self.add_face(['hex', 'top', 'left'],
+                  [['top', 'left', 'v_square'],
+                   ['top', 'left', 'u_boundary'],
+                   [['left'], ['top', 'right', 'v_square']],
+                   [['left', 'top'], ['bottom', 'right', 'v_square']],
+                   [['top'], ['bottom', 'left', 'u_boundary']],
+                   [['top'], ['bottom', 'left', 'v_square']]],
+                  face_type='hexagon', corner=True)
 
 class HexSquareTriTessagon(Tessagon):
   def init_tile_class(self):
