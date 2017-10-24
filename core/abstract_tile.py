c7f0d49df933c850ab1a79b3b8429a81e5128180
Chris Want <cjwant@gmail.com>
2017-10-23 21:35:18 -0600

    Mark public/protected interfaces in tile/abstract_tile.

--- core/abstract_tile.py
+++ core/abstract_tile.py
@@ -52,101 +52,13 @@ class AbstractTile(ValueBlend):
     if 'right' in kwargs:
       self.neighbors['right'] = kwargs['right']
 
-  # A couple of abstract methods that will be useful for finding
-  # and setting the vertices and faces on a tile
-  def get_nested_list_value(self, nested_list, index_path):
-    if not isinstance(index_path, list):
-      return nested_list[index_path]
-    value = nested_list
-    for index in index_path:
-      value = value[index]
-    return value
-
-  def set_nested_list_value(self, nested_list, index_path, value):
-    if not isinstance(index_path, list):
-      nested_list[index_path] = value
-      return
-    reference = nested_list
-    for index in index_path[0:-1]:
-      reference = reference[index]
-    reference[index_path[-1]] = value
-
-  def neighbor_path(self, neighbor_keys):
-    # Note: it is assumed that len(neighbor_keys) in [1, 2]
-    # if len(neighbor_keys) == 1, the neighbor meets on an edge
-    # if len(neighbor_keys) == 2, the neighbor meets at a corner,
-    #   and are diagonal for each other, e.g., ['left', 'top']
-    # If the boundary is twisted, need to be careful because
-    # left and become right, or top can become bottom on the
-    # other side of the twisted boundary: try to traverse the
-    # non-twisted boundary first to make the math easier
-    if len(neighbor_keys) < 2: return neighbor_keys
-    if self.should_twist_u(neighbor_keys):
-      if (neighbor_keys[0] in ['top', 'bottom']):
-        return [neighbor_keys[1], neighbor_keys[0]]
-    elif self.should_twist_v(neighbor_keys):
-      if (neighbor_keys[0] in ['left', 'right']):
-        return [neighbor_keys[1], neighbor_keys[0]]
-    return neighbor_keys
-
-  def index_path(self, index_keys, neighbor_keys):
-    path = index_keys
-    if self.should_twist_u(neighbor_keys):
-      path = self.u_flip(path)
-    if self.should_twist_v(neighbor_keys):
-      path = self.v_flip(path)
-    return path
-    
   def get_neighbor_tile(self, neighbor_keys):
     tile = self
-    for key in self.neighbor_path(neighbor_keys):
+    for key in self._neighbor_path(neighbor_keys):
       if not tile.neighbors[key]:
         return None
       tile = tile.neighbors[key]
     return tile
-
-  def swap_value(self, index_path, val1, val2):
-    # abstract function to swap two values in a list
-    # e.g., 'left' and 'right' in u_flip below
-    if isinstance(index_path, list):
-      return [self.swap_value(u, val1, val2) for u in index_path]
-    if index_path == val1: return val2
-    if index_path == val2: return val1
-    return index_path
-
-  def u_flip(self, index_path):
-    # swap each left with right (and vice versa) in list
-    if not self.u_symmetric: return index_path
-    return self.swap_value(index_path, 'left', 'right')
-
-  def v_flip(self, index_path):
-    # swap each top with bottom (and vice versa) in list
-    if not self.v_symmetric: return index_path
-    return self.swap_value(index_path, 'bottom', 'top')
-
-  def v_index(self, index_path):
-    # find either 'top' or 'bottom' in the list
-    if ('bottom' in index_path): return 'bottom'
-    if ('top' in index_path): return 'top'
-    raise ValueError("no v_index found in %s" % (index_path))
-
-  def u_index(self, index_path):
-    # find either 'right' or 'left' in the list
-    if ('left' in index_path): return 'left'
-    if ('right' in index_path): return 'right'
-    raise ValueError("no u_index found in %s" % (index_path))
-
-  def should_twist_u(self, neighbor_keys):
-    # e.g., twist['bottom'] is True, and neigbor_keys has 'bottom' in it
-    for twist in ['top', 'bottom']:
-      if self.twist[twist] and twist in neighbor_keys: return True
-    return False
-
-  def should_twist_v(self, neighbor_keys):
-    # e.g., twist['left'] is True, and neigbor_keys has 'left' in it
-    for twist in ['left', 'right']:
-      if self.twist[twist] and twist in neighbor_keys: return True
-    return False
 
   def inspect(self, **kwargs):
     # For debugging topology
@@ -161,7 +73,7 @@ class AbstractTile(ValueBlend):
       if self.neighbors[key]:
         tile = self.neighbors[key]
         if tile.id:
-          print("    - %s" % (self.neighbor_str(key)))
+          print("    - %s" % (self._neighbor_str(key)))
     print("  - corners: (%2.4f, %2.4f)  (%2.4f, %2.4f)" %
           tuple(self.corners[2] + self.corners[3]))
     print("             (%2.4f, %2.4f)  (%2.4f, %2.4f)" %
@@ -169,7 +81,97 @@ class AbstractTile(ValueBlend):
     print("  - twist:", self.twist)
     print('')
 
-  def neighbor_str(self, key):
+  #### Below are protected
+
+  # A couple of abstract methods that will be useful for finding
+  # and setting the vertices and faces on a tile
+  def _get_nested_list_value(self, nested_list, index_path):
+    if not isinstance(index_path, list):
+      return nested_list[index_path]
+    value = nested_list
+    for index in index_path:
+      value = value[index]
+    return value
+
+  def _set_nested_list_value(self, nested_list, index_path, value):
+    if not isinstance(index_path, list):
+      nested_list[index_path] = value
+      return
+    reference = nested_list
+    for index in index_path[0:-1]:
+      reference = reference[index]
+    reference[index_path[-1]] = value
+
+  def _neighbor_path(self, neighbor_keys):
+    # Note: it is assumed that len(neighbor_keys) in [1, 2]
+    # if len(neighbor_keys) == 1, the neighbor meets on an edge
+    # if len(neighbor_keys) == 2, the neighbor meets at a corner,
+    #   and are diagonal for each other, e.g., ['left', 'top']
+    # If the boundary is twisted, need to be careful because
+    # left and become right, or top can become bottom on the
+    # other side of the twisted boundary: try to traverse the
+    # non-twisted boundary first to make the math easier
+    if len(neighbor_keys) < 2: return neighbor_keys
+    if self._should_twist_u(neighbor_keys):
+      if (neighbor_keys[0] in ['top', 'bottom']):
+        return [neighbor_keys[1], neighbor_keys[0]]
+    elif self._should_twist_v(neighbor_keys):
+      if (neighbor_keys[0] in ['left', 'right']):
+        return [neighbor_keys[1], neighbor_keys[0]]
+    return neighbor_keys
+
+  def _index_path(self, index_keys, neighbor_keys):
+    path = index_keys
+    if self._should_twist_u(neighbor_keys):
+      path = self._u_flip(path)
+    if self._should_twist_v(neighbor_keys):
+      path = self._v_flip(path)
+    return path
+    
+  def _swap_value(self, index_path, val1, val2):
+    # abstract function to swap two values in a list
+    # e.g., 'left' and 'right' in u_flip below
+    if isinstance(index_path, list):
+      return [self._swap_value(u, val1, val2) for u in index_path]
+    if index_path == val1: return val2
+    if index_path == val2: return val1
+    return index_path
+
+  def _u_flip(self, index_path):
+    # swap each left with right (and vice versa) in list
+    if not self.u_symmetric: return index_path
+    return self._swap_value(index_path, 'left', 'right')
+
+  def _v_flip(self, index_path):
+    # swap each top with bottom (and vice versa) in list
+    if not self.v_symmetric: return index_path
+    return self._swap_value(index_path, 'bottom', 'top')
+
+  def _v_index(self, index_path):
+    # find either 'top' or 'bottom' in the list
+    if ('bottom' in index_path): return 'bottom'
+    if ('top' in index_path): return 'top'
+    raise ValueError("no v_index found in %s" % (index_path))
+
+  def _u_index(self, index_path):
+    # find either 'right' or 'left' in the list
+    if ('left' in index_path): return 'left'
+    if ('right' in index_path): return 'right'
+    raise ValueError("no u_index found in %s" % (index_path))
+
+  def _should_twist_u(self, neighbor_keys):
+    # e.g., twist['bottom'] is True, and neigbor_keys has 'bottom' in it
+    for twist in ['top', 'bottom']:
+      if self.twist[twist] and twist in neighbor_keys: return True
+    return False
+
+  def _should_twist_v(self, neighbor_keys):
+    # e.g., twist['left'] is True, and neigbor_keys has 'left' in it
+    for twist in ['left', 'right']:
+      if self.twist[twist] and twist in neighbor_keys: return True
+    return False
+
+  def _neighbor_str(self, key):
     tile = self.neighbors[key]
     if tile:
       return "%-9s%s" % ("%s:" % (key), tile.id)
--- core/tile.py
+++ core/tile.py
@@ -9,34 +9,15 @@ class Tile(AbstractTile):
     self.verts = self.init_verts()
     self.faces = self.init_faces()
 
-  def get_vert(self, index_keys):
-    return self.get_nested_list_value(self.verts, index_keys)
-
-  def set_vert(self, index_keys, value):
-    self.set_nested_list_value(self.verts, index_keys, value)
-
-  def get_face(self, index_keys):
-    return self.get_nested_list_value(self.faces, index_keys)
-
-  def set_face(self, index_keys, value):
-    self.set_nested_list_value(self.faces, index_keys, value)
-
-  def get_neighbor_vert(self, neighbor_keys, index_keys):
-    # See comment about neighbors in AbstractTile
-    tile = self.get_neighbor_tile(neighbor_keys)
-    if not tile:
-      return None
-    return tile.get_vert(self.index_path(index_keys, neighbor_keys))
-
-  # Use the mesh adaptor to create a vertex.
-  # In reality, multiple vertices may get defined if symmetry is declared
   def add_vert(self, index_keys, ratio_u, ratio_v, **kwargs):
-    vert = self.get_vert(index_keys)
+    # Use the mesh adaptor to create a vertex.
+    # In reality, multiple vertices may get defined if symmetry is declared
+    vert = self._get_vert(index_keys)
     if not vert:
       coords = self.f(*self.blend(ratio_u, ratio_v))
       vert = self.mesh_adaptor.create_vert(coords)
 
-      self.set_vert(index_keys, vert)
+      self._set_vert(index_keys, vert)
       if 'vert_type' in kwargs:
         if not kwargs['vert_type'] in self.tessagon.vert_types:
           self.tessagon.vert_types[kwargs['vert_type']] = []
@@ -44,86 +25,36 @@ class Tile(AbstractTile):
 
     # We add additional vertices by flipping 'left', 'right' etc
     # if the tile has some kind of symmetry defined
-    self.create_symmetric_verts(index_keys, ratio_u, ratio_v, **kwargs)
+    self._create_symmetric_verts(index_keys, ratio_u, ratio_v, **kwargs)
     
     # On the boundary, make sure equivalent vertices are set on neighbor tiles
-    self.set_equivalent_neighbor_verts(index_keys, vert, **kwargs)
+    self._set_equivalent_neighbor_verts(index_keys, vert, **kwargs)
 
     return vert
 
-  def create_symmetric_verts(self, index_keys, ratio_u, ratio_v, **kwargs):
-    # The 'symmetry' keyword is just to ensure we don't recurse forever
-    if not 'symmetry' in kwargs:
-      extra_args = { 'symmetry': True}
-      if self.u_symmetric:
-        # Add reflection about u
-        u_flip_keys = self.u_flip(index_keys)
-        self.add_vert(u_flip_keys, 1.0 - ratio_u, ratio_v,
-                      **{**kwargs, **extra_args})
-        if self.v_symmetric:
-          # Add diagonally across
-          uv_flip_keys = self.v_flip(u_flip_keys)
-          self.add_vert(uv_flip_keys, 1.0 - ratio_u, 1.0 - ratio_v,
-                        **{**kwargs, **extra_args})
-      if self.v_symmetric:
-        # Add reflection about v
-        v_flip_keys = self.v_flip(index_keys)
-        self.add_vert(v_flip_keys, ratio_u, 1.0 - ratio_v,
-                      **{**kwargs, **extra_args})
-
-  def set_equivalent_neighbor_verts(self, index_keys, vert, **kwargs):
-    if 'u_boundary' in kwargs:
-      self.set_u_equivalent_vert(index_keys, vert, **kwargs)
-    if 'v_boundary' in kwargs:
-      self.set_v_equivalent_vert(index_keys, vert, **kwargs)
-    if 'corner' in kwargs:
-      self.set_u_equivalent_vert(index_keys, vert, **kwargs)
-      self.set_v_equivalent_vert(index_keys, vert, **kwargs)
-      self.set_uv_equivalent_vert(index_keys, vert, **kwargs)
-
-  # On boundary, the vert on a neighbor is equivalent to this vert
-  # This is usually only called indirectly via add_vert, but check out
-  # PythagoreanTile for an example of direct usage
   def set_equivalent_vert(self, neighbor_keys, index_keys, vert, **kwargs):
+    # On boundary, the vert on a neighbor is equivalent to this vert
+    # This is usually only called indirectly via add_vert, but check out
+    # PythagoreanTile for an example of direct usage
     if not vert:
       return None
     tile = self.get_neighbor_tile(neighbor_keys)
     if not tile:
       return None
 
-    tile.set_vert(self.index_path(index_keys, neighbor_keys), vert)
+    tile._set_vert(self._index_path(index_keys, neighbor_keys), vert)
 
-  # Handle vert on left/right boundary
-  def set_u_equivalent_vert(self, index_keys, vert, **kwargs):
-    u_index = self.u_index(index_keys)
-    u_flip_keys = self.u_flip(index_keys)
-    self.set_equivalent_vert([u_index], u_flip_keys, vert, **kwargs)
-
-  # Handle vert on top/bottom boundary
-  def set_v_equivalent_vert(self, index_keys, vert, **kwargs):
-    v_index = self.v_index(index_keys)
-    v_flip_keys = self.v_flip(index_keys)
-    self.set_equivalent_vert([v_index], v_flip_keys, vert, **kwargs)
-
-  # Handle vert on corner, equivalent to vert on diagonal tile
-  def set_uv_equivalent_vert(self, index_keys, vert, **kwargs):
-    u_index = self.u_index(index_keys)
-    v_index = self.v_index(index_keys)
-    u_flip_keys = self.u_flip(index_keys)
-    uv_flip_keys = self.v_flip(u_flip_keys)
-    self.set_equivalent_vert([u_index, v_index], uv_flip_keys, vert, **kwargs)
-
-  # Use the mesh adaptor to create a face.
-  # In reality, multiple faces may get defined if symmetry is declared
   def add_face(self, index_keys, vert_index_keys_list, **kwargs):
-    if self.get_face(index_keys):
+    # Use the mesh adaptor to create a face.
+    # In reality, multiple faces may get defined if symmetry is declared
+    if self._get_face(index_keys):
       return None
 
-    verts = self.get_verts_from_list(vert_index_keys_list)
+    verts = self._get_verts_from_list(vert_index_keys_list)
     if not verts: return None
 
     face = self.mesh_adaptor.create_face(verts)
-    self.set_face(index_keys, face)
+    self._set_face(index_keys, face)
 
     # The tessagon might keep a list of specific face types
     if 'face_type' in kwargs:
@@ -133,83 +64,154 @@ class Tile(AbstractTile):
 
     # We add additional faces by flipping 'left', 'right' etc
     # if the tile has some kind of symmetry defined
-    self.create_symmetric_faces(index_keys, vert_index_keys_list, **kwargs)
+    self._create_symmetric_faces(index_keys, vert_index_keys_list, **kwargs)
 
     # On the boundary, make sure equivalent faces are set on neighbor tiles
-    self.set_equivalent_neighbor_faces(index_keys, face, **kwargs)
+    self._set_equivalent_neighbor_faces(index_keys, face, **kwargs)
 
     return face
 
-  def get_verts_from_list(self, vert_index_keys_list):
-    verts = []
-    for vert_index_keys in vert_index_keys_list:
-      if isinstance(vert_index_keys[0], list):
-        vert = self.get_neighbor_vert(vert_index_keys[0], vert_index_keys[1])
-      else:
-        vert = self.get_vert(vert_index_keys)
-      if not vert: return None
-      verts.append(vert)
+  def set_equivalent_face(self, neighbor_keys, index_keys, face, **kwargs):
+    # On boundary, the face on a neighbor is equivalent to this face
+    # This is usually only called indirectly via add_face, but check out
+    # PythagoreanTile for an example of direct usage
+    tile = self.get_neighbor_tile(neighbor_keys)
+    if not tile:
+      return None
+    tile._set_face(self._index_path(index_keys, neighbor_keys), face)
 
-    return verts
-    
-  def create_symmetric_faces(self, index_keys, vert_index_keys_list, **kwargs):
+  ### Below are protected
+
+  def _get_vert(self, index_keys):
+    return self._get_nested_list_value(self.verts, index_keys)
+
+  def _set_vert(self, index_keys, value):
+    self._set_nested_list_value(self.verts, index_keys, value)
+
+  def _get_face(self, index_keys):
+    return self._get_nested_list_value(self.faces, index_keys)
+
+  def _set_face(self, index_keys, value):
+    self._set_nested_list_value(self.faces, index_keys, value)
+
+  def _get_neighbor_vert(self, neighbor_keys, index_keys):
+    # See comment about neighbors in AbstractTile
+    tile = self.get_neighbor_tile(neighbor_keys)
+    if not tile:
+      return None
+    return tile._get_vert(self._index_path(index_keys, neighbor_keys))
+
+  def _create_symmetric_verts(self, index_keys, ratio_u, ratio_v, **kwargs):
     # The 'symmetry' keyword is just to ensure we don't recurse forever
     if not 'symmetry' in kwargs:
       extra_args = { 'symmetry': True}
       if self.u_symmetric:
         # Add reflection about u
-        u_flip_keys = self.u_flip(index_keys)
-        u_flip_vert_index_keys_list = self.u_flip(vert_index_keys_list)
+        u_flip_keys = self._u_flip(index_keys)
+        self.add_vert(u_flip_keys, 1.0 - ratio_u, ratio_v,
+                      **{**kwargs, **extra_args})
+        if self.v_symmetric:
+          # Add diagonally across
+          uv_flip_keys = self._v_flip(u_flip_keys)
+          self.add_vert(uv_flip_keys, 1.0 - ratio_u, 1.0 - ratio_v,
+                        **{**kwargs, **extra_args})
+      if self.v_symmetric:
+        # Add reflection about v
+        v_flip_keys = self._v_flip(index_keys)
+        self.add_vert(v_flip_keys, ratio_u, 1.0 - ratio_v,
+                      **{**kwargs, **extra_args})
+
+  def _set_equivalent_neighbor_verts(self, index_keys, vert, **kwargs):
+    if 'u_boundary' in kwargs:
+      self._set_u_equivalent_vert(index_keys, vert, **kwargs)
+    if 'v_boundary' in kwargs:
+      self._set_v_equivalent_vert(index_keys, vert, **kwargs)
+    if 'corner' in kwargs:
+      self._set_u_equivalent_vert(index_keys, vert, **kwargs)
+      self._set_v_equivalent_vert(index_keys, vert, **kwargs)
+      self._set_uv_equivalent_vert(index_keys, vert, **kwargs)
+
+  # Handle vert on left/right boundary
+  def _set_u_equivalent_vert(self, index_keys, vert, **kwargs):
+    u_index = self._u_index(index_keys)
+    u_flip_keys = self._u_flip(index_keys)
+    self.set_equivalent_vert([u_index], u_flip_keys, vert, **kwargs)
+
+  # Handle vert on top/bottom boundary
+  def _set_v_equivalent_vert(self, index_keys, vert, **kwargs):
+    v_index = self._v_index(index_keys)
+    v_flip_keys = self._v_flip(index_keys)
+    self.set_equivalent_vert([v_index], v_flip_keys, vert, **kwargs)
+
+  # Handle vert on corner, equivalent to vert on diagonal tile
+  def _set_uv_equivalent_vert(self, index_keys, vert, **kwargs):
+    u_index = self._u_index(index_keys)
+    v_index = self._v_index(index_keys)
+    u_flip_keys = self._u_flip(index_keys)
+    uv_flip_keys = self._v_flip(u_flip_keys)
+    self.set_equivalent_vert([u_index, v_index], uv_flip_keys, vert, **kwargs)
+
+  def _get_verts_from_list(self, vert_index_keys_list):
+    verts = []
+    for vert_index_keys in vert_index_keys_list:
+      if isinstance(vert_index_keys[0], list):
+        vert = self._get_neighbor_vert(vert_index_keys[0], vert_index_keys[1])
+      else:
+        vert = self._get_vert(vert_index_keys)
+      if not vert: return None
+      verts.append(vert)
+
+    return verts
+    
+  def _create_symmetric_faces(self, index_keys, vert_index_keys_list, **kwargs):
+    # The 'symmetry' keyword is just to ensure we don't recurse forever
+    if not 'symmetry' in kwargs:
+      extra_args = { 'symmetry': True}
+      if self.u_symmetric:
+        # Add reflection about u
+        u_flip_keys = self._u_flip(index_keys)
+        u_flip_vert_index_keys_list = self._u_flip(vert_index_keys_list)
         self.add_face(u_flip_keys, u_flip_vert_index_keys_list,
                       **{**kwargs, **extra_args})
         if self.v_symmetric:
           # Add diagonally across
-          uv_flip_keys = self.v_flip(u_flip_keys)
-          uv_flip_vert_index_keys_list = self.v_flip(u_flip_vert_index_keys_list)
+          uv_flip_keys = self._v_flip(u_flip_keys)
+          uv_flip_vert_index_keys_list = self._v_flip(u_flip_vert_index_keys_list)
           self.add_face(uv_flip_keys, uv_flip_vert_index_keys_list,
                         **{**kwargs, **extra_args})
       if self.v_symmetric:
         # Add reflection about v
-        v_flip_keys = self.v_flip(index_keys)
-        v_flip_vert_index_keys_list = self.v_flip(vert_index_keys_list)
+        v_flip_keys = self._v_flip(index_keys)
+        v_flip_vert_index_keys_list = self._v_flip(vert_index_keys_list)
         self.add_face(v_flip_keys, v_flip_vert_index_keys_list,
                       **{**kwargs, **extra_args})
 
-  def set_equivalent_neighbor_faces(self, index_keys, face, **kwargs):
+  def _set_equivalent_neighbor_faces(self, index_keys, face, **kwargs):
     if 'u_boundary' in kwargs:
-      self.set_u_equivalent_face(index_keys, face, **kwargs)
+      self._set_u_equivalent_face(index_keys, face, **kwargs)
     if 'v_boundary' in kwargs:
-      self.set_v_equivalent_face(index_keys, face, **kwargs)
+      self._set_v_equivalent_face(index_keys, face, **kwargs)
     if 'corner' in kwargs:
-      self.set_u_equivalent_face(index_keys, face, **kwargs)
-      self.set_v_equivalent_face(index_keys, face, **kwargs)
-      self.set_uv_equivalent_face(index_keys, face, **kwargs)
-
-  # On boundary, the face on a neighbor is equivalent to this face
-  # This is usually only called indirectly via add_face, but check out
-  # PythagoreanTile for an example of direct usage
-  def set_equivalent_face(self, neighbor_keys, index_keys, face, **kwargs):
-    tile = self.get_neighbor_tile(neighbor_keys)
-    if not tile:
-      return None
-    tile.set_face(self.index_path(index_keys, neighbor_keys), face)
+      self._set_u_equivalent_face(index_keys, face, **kwargs)
+      self._set_v_equivalent_face(index_keys, face, **kwargs)
+      self._set_uv_equivalent_face(index_keys, face, **kwargs)
 
   # Handle face on left/right boundary
-  def set_u_equivalent_face(self, index_keys, face, **kwargs):
-    u_index = self.u_index(index_keys)
-    u_flip_keys = self.u_flip(index_keys)
+  def _set_u_equivalent_face(self, index_keys, face, **kwargs):
+    u_index = self._u_index(index_keys)
+    u_flip_keys = self._u_flip(index_keys)
     self.set_equivalent_face([u_index], u_flip_keys, face, **kwargs)
 
   # Handle face on top/bottom boundary
-  def set_v_equivalent_face(self, index_keys, face, **kwargs):
-    v_index = self.v_index(index_keys)
-    v_flip_keys = self.v_flip(index_keys)
+  def _set_v_equivalent_face(self, index_keys, face, **kwargs):
+    v_index = self._v_index(index_keys)
+    v_flip_keys = self._v_flip(index_keys)
     self.set_equivalent_face([v_index], v_flip_keys, face, **kwargs)
 
   # Handle face on corner, equivalent to face on diagonal tile
-  def set_uv_equivalent_face(self, index_keys, face, **kwargs):
-    u_index = self.u_index(index_keys)
-    v_index = self.v_index(index_keys)
-    u_flip_keys = self.u_flip(index_keys)
-    uv_flip_keys = self.v_flip(u_flip_keys)
+  def _set_uv_equivalent_face(self, index_keys, face, **kwargs):
+    u_index = self._u_index(index_keys)
+    v_index = self._v_index(index_keys)
+    u_flip_keys = self._u_flip(index_keys)
+    uv_flip_keys = self._v_flip(u_flip_keys)
     self.set_equivalent_face([u_index, v_index], uv_flip_keys, face, **kwargs)
