091a5758e14b037e19005fdba760f9444de1c649
Chris Want <cjwant@gmail.com>
2018-02-24 16:02:19 -0700

    Make test suite PEP8 compliant.

--- tests/core/core_tests_base.py
+++ tests/core/core_tests_base.py
@@ -1,7 +1,18 @@
-import sys, os
+import os
+import sys
+
 this_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(this_dir + '/../../..')
-import tessagon
+
 
 class CoreTestsBase:
-  pass
+    pass
+
+
+class FakeTessagon:
+    def f(self, u, v):
+        return [u, u*v, v]
+
+
+class FakeAdaptor:
+    pass
