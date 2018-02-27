a766c01f71c5d10e026ccf788f1a1d8450799442
Chris Want <cjwant@gmail.com>
2018-02-27 12:43:43 -0700

    Make adaptors flake8/PEP8 compliant.

--- adaptors/blender_adaptor.py
+++ adaptors/blender_adaptor.py
@@ -1,26 +1,27 @@
 import bmesh
 
+
 class BlenderAdaptor:
-  def __init__(self, **kwargs):
-    self.bm = None
+    def __init__(self, **kwargs):
+        self.bm = None
 
-  def create_empty_mesh(self):
-    self.bm = bmesh.new()
+    def create_empty_mesh(self):
+        self.bm = bmesh.new()
 
-  def initialize_colors(self):
-    pass
+    def initialize_colors(self):
+        pass
 
-  def create_vert(self, coords):
-    return self.bm.verts.new(coords)
-  
-  def create_face(self, verts):
-    return self.bm.faces.new(verts)
+    def create_vert(self, coords):
+        return self.bm.verts.new(coords)
 
-  def color_face(self, face, color_index):
-    face.material_index = color_index
+    def create_face(self, verts):
+        return self.bm.faces.new(verts)
 
-  def finish_mesh(self):
-    bmesh.ops.recalc_face_normals(self.bm, faces=self.bm.faces)
+    def color_face(self, face, color_index):
+        face.material_index = color_index
 
-  def get_mesh(self):
-    return self.bm
+    def finish_mesh(self):
+        bmesh.ops.recalc_face_normals(self.bm, faces=self.bm.faces)
+
+    def get_mesh(self):
+        return self.bm
--- adaptors/vtk_adaptor.py
+++ adaptors/vtk_adaptor.py
@@ -1,48 +1,48 @@
 import vtk
 
+
 class VtkAdaptor:
-  def __init__(self, **kwargs):
-    self.point_count = 0
-    self.face_count = 0
-    self.points = None
-    self.polys = None
-    self.poly_data = None
-    self.scalars = None
+    def __init__(self, **kwargs):
+        self.point_count = 0
+        self.face_count = 0
+        self.points = None
+        self.polys = None
+        self.poly_data = None
+        self.scalars = None
 
-  def create_empty_mesh(self):
-    self.pcoords = vtk.vtkFloatArray()
-    self.pcoords.SetNumberOfComponents(3)
-    self.points = vtk.vtkPoints()
-    self.polys = vtk.vtkCellArray()
-    self.poly_data = vtk.vtkPolyData()
+    def create_empty_mesh(self):
+        self.pcoords = vtk.vtkFloatArray()
+        self.pcoords.SetNumberOfComponents(3)
+        self.points = vtk.vtkPoints()
+        self.polys = vtk.vtkCellArray()
+        self.poly_data = vtk.vtkPolyData()
 
-  def initialize_colors(self):
-    self.scalars = vtk.vtkFloatArray()
-    self.scalars.SetNumberOfComponents(1)
-    self.scalars.SetNumberOfTuples(self.face_count)
+    def initialize_colors(self):
+        self.scalars = vtk.vtkFloatArray()
+        self.scalars.SetNumberOfComponents(1)
+        self.scalars.SetNumberOfTuples(self.face_count)
 
-  def create_vert(self, coords):
-    self.pcoords.InsertNextTuple3(*coords)
-    index = self.point_count
-    self.point_count += 1
-    return index
-  
-  def create_face(self, verts):
-    self.polys.InsertNextCell(len(verts), verts)
-    index = self.face_count
-    self.face_count += 1
-    return index
+    def create_vert(self, coords):
+        self.pcoords.InsertNextTuple3(*coords)
+        index = self.point_count
+        self.point_count += 1
+        return index
 
-  def color_face(self, face, color_index):
-    self.scalars.SetTuple1(face, color_index)
+    def create_face(self, verts):
+        self.polys.InsertNextCell(len(verts), verts)
+        index = self.face_count
+        self.face_count += 1
+        return index
 
-  def finish_mesh(self):
-    self.points.SetData(self.pcoords)
-    self.poly_data.SetPoints(self.points)
-    self.poly_data.SetPolys(self.polys)
-    if self.scalars:
-      self.poly_data.GetCellData().SetScalars(self.scalars)
+    def color_face(self, face, color_index):
+        self.scalars.SetTuple1(face, color_index)
 
-  def get_mesh(self):
-    return self.poly_data
+    def finish_mesh(self):
+        self.points.SetData(self.pcoords)
+        self.poly_data.SetPoints(self.points)
+        self.poly_data.SetPolys(self.polys)
+        if self.scalars:
+            self.poly_data.GetCellData().SetScalars(self.scalars)
 
+    def get_mesh(self):
+        return self.poly_data
