055fd0abb9e38e5c9e177a885bddadc7b8474602
Chris Want <cjwant@gmail.com>
2018-03-13 12:56:25 -0600

    Restructuring the project for packaging via pip. (#6)

--- tests/core/core_tests_base.py
+++ tests/core/core_tests_base.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(this_dir + '/../../..')
+sys.path.append(this_dir + '/../..')
 
 from tessagon.core.tile import Tile  # noqa: E402
 
