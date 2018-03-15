aa9e254ffd5177ba4168801df60498219c8309ee
Chris Want <cjwant@gmail.com>
2018-03-15 12:55:18 -0600

    Bump version number.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='tessagon',
-    version='0.3',
+    version='0.4',
     description='Tessellate your favorite 2D manifolds with triangles, ' +
     'hexagons, and other interesting patterns.',
     long_description=long_description,
