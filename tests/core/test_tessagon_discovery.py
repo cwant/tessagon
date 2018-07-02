73126297ed54929b995c19a086e000ba8c054f04
Chris Want <cjwant@gmail.com>
2018-07-02 16:26:55 -0600

    Rename `all` to `find_all` to prevent potential collision with keyword.

--- tests/core/test_tessagon_discovery.py
+++ tests/core/test_tessagon_discovery.py
@@ -7,41 +7,41 @@ from tessagon.types.square_tessagon import SquareTessagon
 
 class TestTessagonDiscovery(CoreTestsBase):
     def test_find_regular(self):
-        all = TessagonDiscovery()
-        regular = all.with_classification('regular')
+        find_all = TessagonDiscovery()
+        regular = find_all.with_classification('regular')
         assert set(regular.to_list()) == set([HexTessagon,
                                               TriTessagon,
                                               SquareTessagon])
         assert regular.count() == 3
 
     def test_inverse(self):
-        all = TessagonDiscovery()
-        regular = all.with_classification('regular')
+        find_all = TessagonDiscovery()
+        regular = find_all.with_classification('regular')
         inverse = regular.inverse()
 
-        all_count = all.count()
-        assert all_count >= 23
+        find_all_count = find_all.count()
+        assert find_all_count >= 23
         regular_count = regular.count()
         assert regular_count == 3
-        assert inverse.count() + regular_count == all_count
+        assert inverse.count() + regular_count == find_all_count
 
         assert set(regular.to_list()) | set(inverse.to_list()) == \
-            set(all.to_list())
+            set(find_all.to_list())
 
     def test_addition(self):
-        all = TessagonDiscovery()
-        regular = all.with_classification('regular')
+        find_all = TessagonDiscovery()
+        regular = find_all.with_classification('regular')
         inverse = regular.inverse()
 
-        assert set((regular + inverse).to_list()) == set(all.to_list())
+        assert set((regular + inverse).to_list()) == set(find_all.to_list())
 
     def test_subtraction(self):
-        all = TessagonDiscovery()
-        regular = all.with_classification('regular')
+        find_all = TessagonDiscovery()
+        regular = find_all.with_classification('regular')
         inverse = regular.inverse()
 
-        assert set((all - regular).to_list()) == set(inverse.to_list())
-        assert (all - all).to_list() == []
+        assert set((find_all - regular).to_list()) == set(inverse.to_list())
+        assert (find_all - find_all).to_list() == []
 
     def test_with_color_pattern(self):
         color = TessagonDiscovery().with_color_patterns()
